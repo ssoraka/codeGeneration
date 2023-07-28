e0024aeb130291dcfd29862c1c4895c73d54b1b3
ssoraka <ssoraka@student.21-school.ru>
2023-07-28 17:22:03 +0300

    learn change jdk tree

--- processor/src/main/java/my/processor/Main.java
+++ processor/src/main/java/my/processor/Main.java
@@ -1,7 +1,16 @@
 package my.processor;
 
+import com.sun.source.util.Trees;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
+
+import javax.tools.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.util.List;
 
 public class Main {
 
@@ -16,7 +25,30 @@ public class Main {
             "}";
 
     public static void main(String[] args) {
-        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
-        parser.setSource(a.toCharArray());
+//        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+//        parser.setSource(a.toCharArray());
+
+
+        String clazzName = "HelloWorld";
+        String code = "public class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World!\"); ru.example.Main.printHelloWorld(); } }";
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+        URI uri = URI.create("string:///" + "HelloWorld" + JavaFileObject.Kind.SOURCE.extension);
+        SimpleJavaFileObject source = new SimpleJavaFileObject(uri, JavaFileObject.Kind.SOURCE) {
+            public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+                return code;
+            }
+        };
+
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+//        DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
+//        List<String> compilerOptions = List.of("-Xlint:unchecked");
+//        List<SimpleJavaFileObject> sources = List.of(source);
+//        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, collector, compilerOptions, null, sources);
+//
+//        Trees instance = Trees.instance(task);
+
     }
 }
--- processorApply/src/main/java/my/example/Test.java
+++ processorApply/src/main/java/my/example/Test.java
@@ -9,7 +9,6 @@ public class Test {
 
     @MyAnnotation
     public static void main(String[] args) {
-//      java  -javaagent:transformator/target/transformator-1.0-SNAPSHOT.jar -jar processorApply/target/processorApply-1.0-SNAPSHOT.jar
         System.out.println("hello world");
     }
 }
